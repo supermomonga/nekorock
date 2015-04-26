ef0c55be6a5d8c477a5a5ab748c068dc118f0bdf
supermomonga <hi@supermomonga.com>
2015-04-27 00:38:43 +0900

    hai

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.1.1"]
+                 [org.clojure/core.typed "0.2.84"]]
   :main ^:skip-aot nekorock.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/nekorock/core.clj
+++ src/nekorock/core.clj
@@ -1,7 +1,25 @@
 (ns nekorock.core
+  (:require [clojure.core.typed :as t]
+            [clj-http.client :as c])
   (:gen-class))
+
+
+(def h {"User-Agent"
+        "Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"})
+
+(t/ann login [String, String -> String])
+(defn login [username password]
+  "Login to mobage"
+  (println username)
+  (-> (c/get "https://ssl.sp.mbga.jp/_lg" {:headers h})
+      :orig-content-encoding))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println
+   (login (System/getenv "MOBAGE_USERNAME") (System/getenv "MOBAGE_PASSWORD")))
+  (println "Hello, World!")
+  (println "oh,"))
+
+
