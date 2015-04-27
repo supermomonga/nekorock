adeb527ec630e632c6853a9b46972c332de8604f
supermomonga <hi@supermomonga.com>
2015-04-28 03:47:26 +0900

    use enlive to parse html

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.84"]
                  [clj-http "1.1.1"]
-                 [org.clojure/core.typed "0.2.84"]]
+                 [enlive "1.1.5"]]
   :main ^:skip-aot nekorock.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/nekorock/imas.clj
+++ src/nekorock/imas.clj
@@ -1,6 +1,7 @@
 (ns nekorock.imas
   (:require [clojure.core.typed :as t]
-            [clj-http.client :as c])
+            [clj-http.client :as c]
+            [net.cgrand.enlive-html :as e])
   (:gen-class))
 
 (def h {"User-Agent" "Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"})
@@ -10,4 +11,12 @@
   "Login to mobage"
   (println username)
   (-> (c/get "https://ssl.sp.mbga.jp/_lg" {:headers h :cookie-store cs})
-      :orig-content-encoding))
+      :body
+      (java.io.StringReader.)
+      (e/html-resource)
+      (as-> it
+          (hash-map :login_history_id (-> (e/select it [[:input (e/attr= :name "login_history_id")]]) first :attrs :value)
+                    :post_login_redirect_uri (-> (e/select it [[:input (e/attr= :name "post_login_redirect_uri")]]) first :attrs :value)))
+      (println)
+      (str "hi")))
+
