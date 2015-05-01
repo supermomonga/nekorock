1989df79c7e1eed5bbe227f430734e0f86c3aad8
supermomonga <hi@supermomonga.com>
2015-05-02 05:15:05 +0900

    Add wrapper functions of http get/post request

--- src/nekorock/imas.clj
+++ src/nekorock/imas.clj
@@ -6,10 +6,20 @@
 
 (def h {"User-Agent" "Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"})
 
+(defn http-get
+  "Like #'request, but sets the :method and :url as appropriate."
+  [url cs & [req]]
+  (c/get url (into {:headers h :cookie-store cs} req)))
+
+(defn http-post
+  "Like #'request, but sets the :method and :url as appropriate."
+  [url cs & [req]]
+  (c/post url (into {:headers h :cookie-store cs} req)))
+
 (def imas-url [])
 
 (defn get-remote-image-bytes [cs url]
-  ((c/get url {:headers h :cookie-store cs :as :byte-array}) :body))
+  ((http-get url cs {:as :byte-array}) :body))
 
 (defn save-captcha [bytes]
   (with-open [w (clojure.java.io/output-stream "captcha.jpg")]
@@ -53,7 +63,7 @@
 (defn login [cs username password captcha-apikey]
   "Login to mobage"
   (println username)
-  (let [params (-> (c/get "http://sp.mbga.jp/_lg" {:headers h :cookie-store cs})
+  (let [params (-> (http-get "http://sp.mbga.jp/_lg" cs)
                    :body
                    (e/html-snippet)
                    (as-> it
@@ -84,14 +94,10 @@
     ;; (save-captcha (get-remote-image-bytes cs (params "captcha_url")))
     (let [captcha (solve-captcha captcha-apikey (get-remote-image-bytes cs (params "captcha_url")))]
       ;; (println (into params {"captcha" captcha}))
-      (-> (c/post "https://connect.mobage.jp/login" {:headers h
-                                                     :cookie-store cs
-                                                     :form-params (into params {"captcha" captcha})})
+      (-> (http-post "https://connect.mobage.jp/login" cs {:form-params (into params {"captcha" captcha})})
           (:body)
           (println))))
-  (-> (c/get "http://sp.pf.mbga.jp/12008305/?guid=ON&url=http%3A%2F%2F125.6.169.35%2Fidolmaster%2Fresults%2Findex%3Fl_frm%3DMypage_1%26rnd%3D956286586"
-             {:headers h
-              :cookie-store cs})
+  (-> (http-get "http://sp.pf.mbga.jp/12008305/?guid=ON&url=http%3A%2F%2F125.6.169.35%2Fidolmaster%2Fresults%2Findex%3Fl_frm%3DMypage_1%26rnd%3D956286586" cs)
       (:body)
       (e/html-snippet)
       (-> (e/select [[:a (e/attr-starts :href "http://mbga.jp/.maf0f/_u?u=" )]]) first :attrs :href
@@ -101,8 +107,7 @@
 
 (defn logout [cs username password]
   "Logout from mobage"
-  (-> (c/get "http://sp.mbga.jp/_logout_confirm" {:headers h :cookie-store cs})))
-
+  (-> (http-get "http://sp.mbga.jp/_logout_confirm" cs)))
 
 ;; https://connect.mobage.jp/login?post_login_redirect_uri=https%3A%2F%2Fconnect.mobage.jp%2Fconnect%2F1.0%2Fservices%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3Dportal-4%26scope%3Dopenid%26redirect_uri%3Dhttps%253A%252F%252Fssl.sp.mbga.jp%252F_connect_login_callback%26state%3D1397494.UcH63tDI990jxtrjNKUxKIwf.1430163291%26display%3Dtouch%26prompt%3Dconsent&sig=3ea53947c5c02b8b18ee6e084c0d5e13af51df73&iat=1430163291&seed=1s6qHybMAyw&display=touch
 ;; https://connect.mobage.jp/login?post_login_redirect_uri=https://connect.mobage.jp/connect/1.0/services/authorize?response_type=code&client_id=portal-4&scope=openid&redirect_uri=https%3A%2F%2Fssl.sp.mbga.jp%2F_connect_login_callback&state=1397494.UcH63tDI990jxtrjNKUxKIwf.1430163291&display=touch&prompt=consent&sig=3ea53947c5c02b8b18ee6e084c0d5e13af51df73&iat=1430163291&seed=1s6qHybMAyw&display=touch
